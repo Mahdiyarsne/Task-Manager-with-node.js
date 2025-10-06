a40a0e8738d57a8a27d83e2353c067670331e316
Mahdiyar <mahdiyarsne98@gmail.com>
2025-10-06 14:11:08 +0330

    Database added with mvc folders

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-const port = 3000;
+const port = 4000;
+
+//mahdiyarsne98
+//mahdiyarsne1998
+//mongodb+srv://mahdiyarsne98:<db_password>@cluster0.1xtd9ic.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+async function connectDb() {
+  try {
+    await mongoose.connect(
+      'mongodb+srv://mahdiyarsne98:mahdiyarsne1998@cluster0.1xtd9ic.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+    );
+    console.log('Database connnected successfully');
+  } catch (error) {
+    console.log('Failed ro connect database', error);
+  }
+}
+
+connectDb();
 
 app.listen(port, () => {
   console.log('server is running');
