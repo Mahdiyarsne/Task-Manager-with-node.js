e2a906c96e5f3e0b267d21fbd3c98d3aa0d42c84
Mahdiyar <mahdiyarsne98@gmail.com>
2025-10-08 13:09:26 +0330

    jwt added

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,15 +1,24 @@
 const User = require('../models/user.model');
+const jwt = require('jsonwebtoken');
 
-const createUser = async (req, res) => {
+const registerUser = async (req, res) => {
   try {
     const { email, password, fullName } = req.body;
 
     const newUser = new User({ email, password, fullName });
 
+    const payload = {
+      id: newUser._id,
+      email: newUser.email,
+      fullName: newUser.fullName,
+    };
+
+    const accessToken = jwt.sign(payload, 'secert-jwt', { expiresIn: '1d' });
+
     await newUser.save();
 
     res.status(201).json({
-      user: newUser,
+      accessToken,
     });
   } catch (error) {
     res.status(500).json({
@@ -19,5 +28,5 @@ const createUser = async (req, res) => {
 };
 
 module.exports = {
-  createUser,
+  registerUser,
 };
--- routes/user.route.js
+++ routes/user.route.js
@@ -2,6 +2,6 @@ const express = require('express');
 const userController = require('../controllers/user.controller');
 const userRoute = express.Router();
 
-userRoute.post('/', userController.createUser);
+userRoute.post('/', userController.registerUser);
 
 module.exports = userRoute;
